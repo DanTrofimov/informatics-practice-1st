4395de3b31ab73145ea195df4a067220f0e2f691
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-04-11 00:48:10 +0300

    JSON & YAML

---
+++ homework #13 (JSON & YAML)/src/JSONReader.java
@@ -0,0 +1,9 @@
+public class JSONReader {
+    public static void main(String[] args) {
+        /*
+        TODO:  JavaSimple https://www.codeflow.site/ru/article/java__json-simple-example-read-and-write-json
+        TODO: SnakeYAML https://www.baeldung.com/java-snake-yaml
+         */
+        System.out.println("hey");
+    }
+}

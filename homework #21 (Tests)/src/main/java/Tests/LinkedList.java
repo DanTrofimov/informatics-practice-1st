550175ba8579cc78a1d844087dc8f9c5611022a4
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-05-25 09:11:15 +0300

    Tests

---
+++ homework #21 (Tests)/src/main/java/Tests/LinkedList.java
@@ -0,0 +1,131 @@
+package Tests;
+
+import javax.management.ListenerNotFoundException;
+
+class LinkedList<T> {
+    public Node head;
+    public int amount;
+
+    // ---- constructor ---
+    public LinkedList() {
+        this.head = null;
+        this.amount = 0;
+    }
+
+    // ---- methods -------
+    public void add(T value) {
+        if (this.head == null) {
+            this.head = new Node(value);
+        } else {
+            Node nextNode;
+            nextNode = this.head;
+            while (nextNode.next != null) {
+                nextNode = nextNode.next;
+            }
+            nextNode.next = new Node(value);
+        }
+        this.amount++;
+    }
+
+    public void addFirst(T value) {
+        Node buffer;
+        if (this.head == null) {
+            this.head = new Node(value);
+        } else {
+            buffer = this.head;
+            this.head = new Node(value);
+            this.head.next = buffer;
+        }
+        this.amount++;
+    }
+
+    public void addAfter(T findValue, T addVal) throws IndexOutOfBoundsException {
+        Node buffer;
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("This list is empty");
+        } else {
+            Node node = this.head;
+            while (node.value != findValue) {
+                node = node.next;
+            }
+            buffer = new Node(addVal);
+            buffer.next = node.next;
+            node.next = buffer;
+        }
+        amount++;
+    }
+
+    public Node getByIndex(int index) {
+        if (index >= amount || this.head == null) {
+            throw new IndexOutOfBoundsException("This index doesn't exist");
+        } else {
+            Node node = this.head;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        }
+    }
+
+    public int size() {
+        return this.amount;
+    }
+
+    public void  removeValue(T value) throws IndexOutOfBoundsException {
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("This list is empty");
+        } else {
+            Node node = this.head;
+            while (node.next.value != value) {
+                node = node.next;
+            }
+            if (node != null) {
+                node.next = node.next.next;
+            }
+            this.amount--;
+        }
+    }
+
+    public void  removeByIndex(int index) throws IndexOutOfBoundsException{
+        if (this.head == null || index >= amount) {
+            throw new IndexOutOfBoundsException("This index doesn't exist");
+        } else {
+            Node node = this.head;
+            for (int i = 0; i < index-1; i++) {
+                node = node.next;
+            }
+            node.next = node.next.next;
+            this.amount--;
+        }
+    }
+
+    public void merge(LinkedList list) {
+        if (this.head == null) {
+            this.head = list.head;
+        } else {
+            Node node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = list.head;
+        }
+        this.amount += list.amount;
+    }
+
+    // ---- Node Class ----
+    public class Node {
+        private Node next;
+        public T value;
+        private int index;
+
+        public Node(T value) {
+            this.next = null;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{value : " + this.value + "; next: " + this.next + "}";
+        }
+    }
+}
